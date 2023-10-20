bcfc5c5206f15affdca8accdd30f2e2087339c32
Omgawade46 <123226570+Omgawade46@users.noreply.github.com>
2023-10-20 12:00:53 +0530

    Update probs.js

--- probs.js
+++ probs.js
@@ -1,13 +1,17 @@
-function Welcome(props) {
-    return <h1>Hello, {props.name}</h1>;
-  }
-  
-  function App() {
-    return (
-      <div>
-        <Welcome name="Sara" />
-        <Welcome name="Cahal" />
-        <Welcome name="Edite" />
-      </div>
-    );
-  }
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+  return <Greeting name="Nathan"/>;
+}
+
+function Greeting(props) {
+  return (
+    <p>
+      Hello! I'm {props.name}.
+      Pleased to meet you!
+    </p>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
